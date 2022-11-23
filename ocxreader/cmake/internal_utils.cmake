2afdf61f5148273dd3e1917f4528b5342a4edb0d
Paul Büchner <paul-buechner@outlook.de>
2022-11-23 03:15:07 +0100

    chore: update cmake setup

--- ocxreader/cmake/internal_utils.cmake
+++ ocxreader/cmake/internal_utils.cmake
@@ -1,13 +1,54 @@
+# Configure OCCT libraries to link with
+macro (target_link_occt_libraries target)
+  foreach (LIB ${OpenCASCADE_LIBRARIES})
+    if (WIN32)
+      if (EXISTS ${OpenCASCADE_LIBRARY_DIR}${DEBUG_SUFFIX}/${LIB}.lib)
+        if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+          set(DEBUG_SUFFIX "d")
+        endif ()
+        set(occt_lib_dir ${OpenCASCADE_LIBRARY_DIR}${DEBUG_SUFFIX}/${LIB}.lib)
+        message(STATUS "Adding ${occt_lib_dir}")
+
+        target_link_libraries(${target} debug ${occt_lib_dir})
+        target_link_libraries(${target} optimized ${occt_lib_dir})
+      else ()
+        message(FATAL_ERROR "Library ${LIB} not found in ${OpenCASCADE_LIBRARY_DIR}${DEBUG_SUFFIX}")
+      endif ()
+    elseif (APPLE)
+      if (EXISTS ${OpenCASCADE_LIBRARY_DIR}/lib${LIB}.dylib)
+        set(occt_lib_dir ${OpenCASCADE_LIBRARY_DIR}/lib${LIB}.dylib)
+        message(STATUS "Adding ${occt_lib_dir}")
+
+        target_link_libraries(${target} debug ${occt_lib_dir})
+        target_link_libraries(${target} optimized ${occt_lib_dir})
+      else ()
+        message(FATAL_ERROR "Library lib${LIB} not found in ${OpenCASCADE_LIBRARY_DIR}")
+      endif ()
+    elseif (UNIX)
+      if (EXISTS ${OpenCASCADE_LIBRARY_DIR}/lib${LIB}.so)
+        set(occt_lib_dir ${OpenCASCADE_LIBRARY_DIR}/lib${LIB}.so)
+        message(STATUS "Adding ${occt_lib_dir}")
+
+        target_link_libraries(${target} debug ${occt_lib_dir})
+        target_link_libraries(${target} optimized ${occt_lib_dir})
+      else ()
+        message(FATAL_ERROR "Library lib${LIB} not found in ${OpenCASCADE_LIBRARY_DIR}")
+      endif ()
+    endif ()
+  endforeach ()
+endmacro ()
+
+
 # Copy target runtime dlls to the build directory by manually specifying the dlls to copy
-macro (copy_runtime_dlls_manual TARGET_NAME)
+macro (copy_runtime_dlls_manual target)
   message(STATUS "OpenCASCADE_BINARY_DIR: ${OpenCASCADE_BINARY_DIR}")
   add_custom_command(
-    TARGET ${TARGET_NAME}
+    TARGET ${target}
     POST_BUILD
     COMMAND
     $<$<CONFIG:DEBUG,RELEASE>:${CMAKE_COMMAND}> -E copy_directory
     $<$<CONFIG:DEBUG>:${OpenCASCADE_BINARY_DIR}d>$<$<NOT:$<CONFIG:DEBUG>>:${OpenCASCADE_BINARY_DIR}>
-    $<TARGET_FILE_DIR:${TARGET_NAME}>)
+    $<TARGET_FILE_DIR:${target}>)
 endmacro ()
 
 
@@ -16,19 +57,19 @@ endmacro ()
 # https://cmake.org/cmake/help/latest/manual/cmake-generator-expressions.7.html#genex:TARGET_RUNTIME_DLLS
 #
 # Not working currently: https://gitlab.kitware.com/cmake/cmake/-/issues/22845
-macro (copy_runtime_dlls_dynamic TARGET_NAME)
+macro (copy_runtime_dlls_dynamic target)
   # Print runtime dlls
   add_custom_command(
-    TARGET ${TARGET_NAME}
+    TARGET ${target}
     POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E echo "Runtime Dlls:
-   $<TARGET_RUNTIME_DLLS:${TARGET_NAME}>")
+   $<TARGET_RUNTIME_DLLS:${target}>")
 
   # Copy dlls to build directory dynamically resolving target runtime dlls
   add_custom_command(
-    TARGET ${TARGET_NAME}
+    TARGET ${target}
     POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_RUNTIME_DLLS:${TARGET_NAME}>
-    $<TARGET_FILE_DIR:${TARGET_NAME}>
+    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_RUNTIME_DLLS:${target}>
+    $<TARGET_FILE_DIR:${target}>
     COMMAND_EXPAND_LISTS)
 endmacro ()
