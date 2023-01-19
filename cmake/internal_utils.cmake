2f134b18f94817007a77cebf682d786a90e0be72
Paul Büchner <paul-buechner@outlook.de>
2023-01-19 02:25:01 +0100

    ci: add install config
    - support to install OCXReader exe
    - genereate 3rd-party license notice
    - custom overlay port to support static and dynamic linked libs (per port config)
    - only load testing lib (gtest) if enabled via `build_tests` option

---
+++ cmake/internal_utils.cmake
@@ -0,0 +1,40 @@
+# Install 3rd party copyright notices
+function (build_3rd_party_copyright)
+  set(LICENSE_3RD_PARTY_FILE ${CMAKE_CURRENT_BINARY_DIR}/LICENSE-3RD-PARTY.txt)
+  # Delete the old file
+  file(REMOVE ${LICENSE_3RD_PARTY_FILE})
+
+  file(GLOB SEARCH_RESULT "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/**/copyright")
+  set(COPYRIGHT_FILES ${SEARCH_RESULT} CACHE INTERNAL "copyright files")
+
+  # Handle duplicate copyright files
+  remove_duplicate_by_file_content("${COPYRIGHT_FILES}" "COPYRIGHT_FILES" "Boost Software License")
+
+  foreach (copyright_file ${COPYRIGHT_FILES})
+    string(REGEX MATCH ".*/(.*)/copyright" _ ${copyright_file})
+    set(LIBRARY_NAME ${CMAKE_MATCH_1})
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------\n")
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "${LIBRARY_NAME}\n")
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------\n")
+    file(READ ${copyright_file} COPYRIGHT_CONTENTS)
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "${COPYRIGHT_CONTENTS}\n")
+  endforeach ()
+endfunction ()
+
+# Remove duplicate from list by content
+function (remove_duplicate_by_file_content list list_name filter_by)
+  set(INCLUDED OFF)
+  foreach (file ${list})
+    file(STRINGS ${file} content)
+    string(FIND "${content}" ${filter_by} FOUND)
+    if (NOT ${FOUND} EQUAL -1)
+      if (INCLUDED)
+        list(REMOVE_ITEM list ${file})
+      else ()
+        set(INCLUDED ON)
+      endif ()
+    endif ()
+  endforeach ()
+  
+  set(${list_name} ${list} PARENT_SCOPE)
+endfunction ()
