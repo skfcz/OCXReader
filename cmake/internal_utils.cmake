c8401ea8e6a8cb0a25e7bcb646d5fd2d27077403
Paul Büchner <45827409+paulbuechner@users.noreply.github.com>
2023-02-23 20:53:56 +0100

    chore(ci): lookup LICENSE files in `deps`

--- cmake/internal_utils.cmake
+++ cmake/internal_utils.cmake
@@ -1,10 +1,11 @@
 # Install 3rd party copyright notices
 function (build_3rd_party_copyright)
   set(LICENSE_3RD_PARTY_FILE ${CMAKE_CURRENT_BINARY_DIR}/LICENSE-3RD-PARTY.txt)
-  # Delete the old file
-  file(REMOVE ${LICENSE_3RD_PARTY_FILE})
+  file(REMOVE ${LICENSE_3RD_PARTY_FILE}) # Delete the old file
 
-  file(GLOB SEARCH_RESULT "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/**/copyright")
+  file(GLOB SEARCH_RESULT
+       "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/**/copyright"
+       "${CMAKE_SOURCE_DIR}/deps/**/LICENSE")
   set(COPYRIGHT_FILES ${SEARCH_RESULT} CACHE INTERNAL "copyright files")
 
   # Handle duplicate copyright files
@@ -12,7 +13,11 @@ function (build_3rd_party_copyright)
 
   foreach (copyright_file ${COPYRIGHT_FILES})
     string(REGEX MATCH ".*/(.*)/copyright" _ ${copyright_file})
+    if ("${CMAKE_MATCH_1}" STREQUAL "")
+      string(REGEX MATCH ".*/(.*)/LICENSE" _ ${copyright_file})
+    endif ()
     set(LIBRARY_NAME ${CMAKE_MATCH_1})
+
     file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------\n")
     file(APPEND ${LICENSE_3RD_PARTY_FILE} "${LIBRARY_NAME}\n")
     file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------\n")
@@ -35,6 +40,5 @@ function (remove_duplicate_by_file_content list list_name filter_by)
       endif ()
     endif ()
   endforeach ()
-  
   set(${list_name} ${list} PARENT_SCOPE)
 endfunction ()
